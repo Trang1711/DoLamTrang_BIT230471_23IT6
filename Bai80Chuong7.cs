37847436959a7feffe69702d340b334a95576c56
Trang1711 <146830798+Trang1711@users.noreply.github.com>
2024-07-25 16:06:22 +0700

    Add files via upload

---
+++ Bai80Chuong7.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Threading;
+
+class Bai80Chuong7
+{
+    static void Main(string[] args)
+    {
+        Thread thread = new Thread(ff);
+        thread.Start();
+
+        // Đợi cho luồng hoàn thành
+        thread.Join();
+
+        // Hiển thị thông báo khi luồng kết thúc
+        Console.WriteLine("Đã thực hiện xong");
+    }
+
+    public static void ff()
+    {
+        Random random = new Random();
+        while (true)
+        {
+            int n = random.Next();
+            if (n % 2025 == 0)
+            {
+                break;
+            }
+            Console.WriteLine(n);
+            Thread.Sleep(100); // Delay 100ms
+        }
+    }
+}
---
+++ Bai81Chuong7.cs
@@ -0,0 +1,30 @@
+﻿
+
+// Đồng bộ sử dụng Thread.Sleep
+using System;
+using System.Text;
+using System.Threading;
+
+class Bai81Chuong7
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Sử dụng Thread.Sleep:");
+        Console.WriteLine("Start");
+        Thread.Sleep(100); // Tạm dừng luồng hiện tại trong 100ms
+        Console.WriteLine("End");
+
+        // Chạy không đồng bộ sử dụng Task.Delay
+        RunAsync().Wait();
+        Console.ReadLine();
+    }
+
+    static async Task RunAsync()
+    {
+        Console.WriteLine("\nSử dụng Task.Delay:");
+        Console.WriteLine("Start");
+        await Task.Delay(100); // Không chặn luồng hiện tại, tiếp tục sau 100ms
+        Console.WriteLine("End");
+    }
+}
